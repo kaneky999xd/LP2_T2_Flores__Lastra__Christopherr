c9f1e659b8e03256d141374cd0c763f8edabfc55
sofia rocio lastra valerio <chrislastra90@gmail.com>
2024-06-23 07:28:35 -0500

    Examen T2 CFL

---
+++ src/main/java/com/example/demo/TemporalTp2CflApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalTp2CflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalTp2CflApplication.class, args);
+	}
+
+}
